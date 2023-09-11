d355dfa6712133d6cc2408cda4499e13a812d14c
Creativity <125555936+overpowered07@users.noreply.github.com>
2023-09-11 15:16:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function topFunction() {
 
 // Typew  Writing effect
 var i = 0;
-var txt = 'Thank you so so much for 155+ subscribers';
+var txt = 'Thank you so so much for 170+ subscribers';
 var speed = 90;
 
 function typeWriter() {
