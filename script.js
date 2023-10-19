1a0cb436e185cd579fe06395b28f0ea0284f7e1d
Stickman Editz <125555936+overpowered07@users.noreply.github.com>
2023-10-19 16:39:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function topFunction() {
 
 // Typew  Writing effect
 var i = 0;
-var txt = 'Thank you so so much for 40+ subscribers';
+var txt = 'Thank you so so much for 45+ subscribers';
 var speed = 90;
 
 function typeWriter() {
